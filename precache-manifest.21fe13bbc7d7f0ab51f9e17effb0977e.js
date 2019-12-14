bd550c384f3da51b1fe79a1219a29f2318eb403c
cbrwizard <cbrwizard@gmail.com>
2019-12-14 22:40:29 +1300

    Updates

---
+++ precache-manifest.21fe13bbc7d7f0ab51f9e17effb0977e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "557c8dc95b0af1df1374ea91210754f6",
+    "url": "/police-arbitrariness-map/index.html"
+  },
+  {
+    "revision": "c409a58f95d9d5132a67",
+    "url": "/police-arbitrariness-map/static/css/2.9ea4d5d3.chunk.css"
+  },
+  {
+    "revision": "afcf9816e813ce639a97",
+    "url": "/police-arbitrariness-map/static/css/main.2c080892.chunk.css"
+  },
+  {
+    "revision": "c409a58f95d9d5132a67",
+    "url": "/police-arbitrariness-map/static/js/2.2ce61a5e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/police-arbitrariness-map/static/js/2.2ce61a5e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "afcf9816e813ce639a97",
+    "url": "/police-arbitrariness-map/static/js/main.4a9de7c7.chunk.js"
+  },
+  {
+    "revision": "88a3ec3e2e16bfce4980",
+    "url": "/police-arbitrariness-map/static/js/runtime-main.7cd6fef3.js"
+  },
+  {
+    "revision": "782154b83b6b419269980b375144b1f7",
+    "url": "/police-arbitrariness-map/static/media/icons-16.782154b8.ttf"
+  },
+  {
+    "revision": "7fb844a70d4ff3aa49441242b4149777",
+    "url": "/police-arbitrariness-map/static/media/icons-16.7fb844a7.woff"
+  },
+  {
+    "revision": "d4f6722b1ac527dd937155d419ec8ddd",
+    "url": "/police-arbitrariness-map/static/media/icons-16.d4f6722b.eot"
+  },
+  {
+    "revision": "90afdd237a469c44c9a1a3b0ff2db197",
+    "url": "/police-arbitrariness-map/static/media/icons-20.90afdd23.eot"
+  },
+  {
+    "revision": "c0e4874e23b8c9100e566ca2ca287128",
+    "url": "/police-arbitrariness-map/static/media/icons-20.c0e4874e.woff"
+  },
+  {
+    "revision": "d1f0f6c1a1c239a03914f7bd06ccfe68",
+    "url": "/police-arbitrariness-map/static/media/icons-20.d1f0f6c1.ttf"
+  }
+]);
