040a0139d00872e911d653eb0986b5b1c23754a1
Ivan Hilkov <strange.mole@gmail.com>
2019-12-14 11:36:13 +0300

    clean up code

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,18 +1,16 @@
 import React from 'react'
-import ReactMapboxGl, { Layer, Feature, GeoJSONLayer } from 'react-mapbox-gl'
+import ReactMapboxGl, { GeoJSONLayer } from 'react-mapbox-gl'
 import geojson from './mo.json'
 
 const ReactMapboxGlMap = ReactMapboxGl({
   accessToken: 'pk.eyJ1Ijoic3RyYW5nZW1vbGUiLCJhIjoiY2o4b3RicGp1MDhqNTMycDEyYm85M3B6OSJ9.EEOG8yH_YtIyLmuHv8zc4g',
 })
 
-console.log(geojson)
-
 const Map = () => {
   const defaultProps = {
     style: 'mapbox://styles/mapbox/streets-v9',
     containerStyle: { height: '100vh', width: '100vw' },
-    center: [37.616844177246094, 55.75201845412423] as [number, number],
+    center: [37.49050140380859, 55.555048994867036] as [number, number],
   }
   return (
     <ReactMapboxGlMap {...defaultProps}>
